60da9a31b7ed0f86357d36547965eeae4dcdc0ce
EnamulRahman <e.r.200@hotmail.co.uk>
2026-02-12 11:10:30 +0000

    edited syntax

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,9 +1,9 @@
 # tells terraform to use AWSA
 
 terraform {
-    required version = ">= 1.6.0"
+    required_version = ">= 1.6.0"
 
-    required providers {
+    required_providers {
         aws = {
             source = "hashicorp/aws"
             version = "~> 5.0" 
