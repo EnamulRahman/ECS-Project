7e6b627a2340814db9010b19a577500711816ea3
EnamulRahman <e.r.200@hotmail.co.uk>
2026-03-05 10:48:46 +0000

    added and completed ECR and ALB modules

---
+++ infra/modules/alb/outputs.tf
@@ -0,0 +1,14 @@
+output "dns_name" {
+  description = "The DNS name of the load balancer"
+  value       = aws_lb.this.dns_name
+}
+
+output "arn" {
+  description = "The ARN of the load balancer"
+  value       = aws_lb.this.arn
+}
+
+output "target_group_arn" {
+  description = "The ARN of the ALB target group"
+  value       = aws_lb_target_group.this.arn
+}
