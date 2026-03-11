24f1715fcd5fb6e7f2b45d6a6dbca93066644059
EnamulRahman <e.r.200@hotmail.co.uk>
2026-03-11 10:19:32 +0000

    amended variables.tf in alb mod

--- infra/modules/alb/variables.tf
+++ infra/modules/alb/variables.tf
@@ -1,5 +1,24 @@
-variable "domain_name" {
-  description = "The domain name for which the certificate should be issued"
+variable "project_name" {
+  description = "Project name"
   type        = string
-  default     = "terraform-aws-modules.modules.tf"
+}
+
+variable "vpc_id" {
+  description = "VPC ID for the load balancer"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "Public subnets for the ALB"
+  type        = list(string)
+}
+
+variable "security_group" {
+  description = "Security group for ALB"
+  type        = string
+}
+
+variable "certificate_arn" {
+  description = "ACM certificate ARN"
+  type        = string
 }
