3b7dfbb154658f132bf0fe8c9f8ec724dd5fd634
EnamulRahman <e.r.200@hotmail.co.uk>
2026-03-12 09:07:11 +0000

    added alb zone id

--- infra/modules/alb/outputs.tf
+++ infra/modules/alb/outputs.tf
@@ -12,3 +12,8 @@ output "target_group_arn" {
   description = "The ARN of the ALB target group"
   value       = aws_lb_target_group.this.arn
 }
+
+output "alb_zone_id" {
+  description = "The hosted zone ID of the ALB (needed for Route53 alias)"
+  value       = aws_lb.this.zone_id
+}
