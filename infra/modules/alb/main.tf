5075ac2d5587361dfb9cbc55c3a81cbee1c36915
EnamulRahman <e.r.200@hotmail.co.uk>
2026-03-12 09:19:58 +0000

    updated alb main.tf

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -1,17 +1,17 @@
 resource "aws_lb" "this" {
-  name               = "memos-alb"
+  name               = "${var.project_name}-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [var.security_group]
   subnets            = var.public_subnets
 
   tags = {
-    Name = "memos-alb"
+    Name = "${var.project_name}-alb"
   }
 }
 
 resource "aws_lb_target_group" "this" {
-  name        = "memos-tg"
+  name        = "${var.project_name}-tg"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
@@ -28,13 +28,32 @@ resource "aws_lb_target_group" "this" {
   }
 }
 
+# HTTP → HTTPS redirect
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# HTTPS listener
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
     target_group_arn = aws_lb_target_group.this.arn
   }
 }
