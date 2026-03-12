076ad840212220ff3fa3b763a1463f105f3e4c4e
EnamulRahman <e.r.200@hotmail.co.uk>
2026-03-12 11:04:38 +0000

    changed ports to 8081

--- infra/modules/security/main.tf
+++ infra/modules/security/main.tf
@@ -1,72 +1,61 @@
-
 # ALB sg
 resource "aws_security_group" "alb" {
-    name        = "${var.project_name}-${var.environment}-alb-sg"
-    description = "Security group for ALB"
-    vpc_id      = var.vpc_id
+  name        = "${var.project_name}-${var.environment}-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = var.vpc_id
 
-
-ingress {
+  ingress {
     description = "allow HTTP from internet"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-}
+  }
 
-ingress {
-  description = "allow HTTPS from internet"
-  from_port   = 8081
-  to_port     = 8081
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress {
+    description = "allow HTTPS from internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-egress {
-    description = "allow all outboound"
+  egress {
+    description = "allow all outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-}
-
-
-tags = merge(var.tags, {
-    Name = "${var.project_name}-${var.environment}-alb-sg"
-})
-}
-
-# ECS sg
-resource "aws_security_group" "ecs" { 
-name        = "${var.project_name}-${var.environment}-ecs-sg"
-  description = "Security group for ECS service"
-  vpc_id      = var.vpc_id
-
-  ingress{
-
-description     = "allow traffic from alb"
-from_port   = 5230
-to_port     = 5230
-protocol        = "tcp"
-security_groups     = [aws_security_group.alb.id]
   }
 
-egress{
-    description = "allow all outbound"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-}
-
-
-tags = merge(var.tags, {
-    name = "${var.project_name}-${var.environment}-ecs-sg"
+  tags = merge(var.tags, {
+    Name = "${var.project_name}-${var.environment}-alb-sg"
   })
 }
 
+# ECS sg
+resource "aws_security_group" "ecs" {
+  name        = "${var.project_name}-${var.environment}-ecs-sg"
+  description = "Security group for ECS service"
+  vpc_id      = var.vpc_id
 
+  ingress {
+    description     = "allow traffic from alb"
+    from_port       = 8081
+    to_port         = 8081
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
 
+  egress {
+    description = "allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-
-
+  tags = merge(var.tags, {
+    Name = "${var.project_name}-${var.environment}-ecs-sg"
+  })
+}
